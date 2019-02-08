b7b84e53d17169b235f268f15e7ae83407e13416
sim511777 <sim51177@gmail.com>
2019-02-08 21:47:20 +0900

    Initial upload

---
+++ JsonTest/FormMain.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+
+namespace JsonTest {
+   public enum JsonSerializerType {
+      DataContractJsonSerializer,
+      JavaScriptSerializer,
+      JsonDotNet,
+   }
+
+   public partial class FormMain : Form {
+      public FormMain() {
+         InitializeComponent();
+         this.cbxJsonType.Items.AddRange(Enum.GetNames(typeof(JsonSerializerType)));
+         this.cbxJsonType.SelectedIndex = 0;
+      }
+
+      private void btnNewObject_Click(object sender, EventArgs e) {
+         this.propertyGrid1.SelectedObject = new MyType();
+      }
+
+      private string ObjectToJson(object obj, JsonSerializerType serializerType, bool indent) {
+         if (serializerType == JsonSerializerType.DataContractJsonSerializer) {
+            var ser = new DataContractJsonSerializer(obj.GetType());
+            using (var ms = new MemoryStream())
+            using (var writer = JsonReaderWriterFactory.CreateJsonWriter(ms, Encoding.UTF8, true, indent)) {
+               ser.WriteObject(writer, obj);
+               writer.Flush();
+               var json = Encoding.UTF8.GetString(ms.ToArray());
+               return json;
+            } 
+         } else if (serializerType == JsonSerializerType.JavaScriptSerializer) {
+            var jss = new JavaScriptSerializer();
+            var json = jss.Serialize(obj);
+            return json;
+         } else if (serializerType == JsonSerializerType.JsonDotNet) {
+            var settings = new JsonSerializerSettings {
+               TypeNameHandling = TypeNameHandling.Auto
+            };
+            var json = JsonConvert.SerializeObject(obj, indent ? Formatting.Indented : Formatting.None, settings);
+            return json;
+         } else {
+            throw new Exception("Invalid Serializer");
+         }
+      }
+
+      private T JsonToObject<T>(string json, JsonSerializerType serializerType, bool indent) {
+         if (serializerType == JsonSerializerType.DataContractJsonSerializer) {
+            var ser = new DataContractJsonSerializer(typeof(T));
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(json))) {
+               var obj = (T)ser.ReadObject(ms);
+               return obj;
+            }
+         } else if (serializerType == JsonSerializerType.JavaScriptSerializer) {
+            var jss = new JavaScriptSerializer();
+            var obj = jss.Deserialize<T>(json);
+            return obj;
+         } else if (serializerType == JsonSerializerType.JsonDotNet) {
+            JsonSerializerSettings a = new JsonSerializerSettings();
+            var obj = JsonConvert.DeserializeObject<T>(json);
+            return obj;
+         } else {
+            throw new Exception("Invalid Serializer");
+         }
+      }
+
+      private void Log(string text) {
+         this.tbxLog.AppendText(text + Environment.NewLine);
+      }
+
+      private void btnToJson_Click(object sender, EventArgs e) {
+         var jsonType = (JsonSerializerType)this.cbxJsonType.SelectedIndex;
+         var indent = this.cbxIndent.Checked;
+         this.Log(string.Format("Object -> Json, JsonType: {0}, Indent: {1}", jsonType, indent));
+         try {
+            var obj = this.propertyGrid1.SelectedObject;
+            var json = this.ObjectToJson(obj, jsonType, indent);
+            this.tbxJson.Text = json;
+         } catch (Exception ex) {
+            this.Log(ex.ToString());
+         }
+         this.Log("========");
+      }
+
+      private void btnFromJson_Click(object sender, EventArgs e) {
+         var jsonType = (JsonSerializerType)this.cbxJsonType.SelectedIndex;
+         var indent = this.cbxIndent.Checked;
+         this.Log(string.Format("Jsont -> Object, JsonType: {0}, Indent: {1}", jsonType, indent));
+         try {
+            var json = this.tbxJson.Text;
+            var obj = this.JsonToObject<MyType>(json, jsonType, indent);
+            this.propertyGrid1.SelectedObject = obj;
+         } catch (Exception ex) {
+            this.Log(ex.ToString());
+         }
+         this.tbxLog.AppendText("========");
+      }
+   }
+}
---
+++ JsonTest/MyType.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonTest {
+   public enum WeekDay {
+      Sunday, Monday, Tuesday, WednesDay, Thursday, Friday, Saturday,
+   }
+
+   public class MyType {
+      public int IntVal { get; set; }
+      public float FloatVal { get; set; }
+      public bool BoolVal { get; set; }
+      public string StringVal { get; set; }
+      public WeekDay WeekDay { get; set; }
+      public int[] IntArr { get; set; }
+      public string[] StringArr { get; set; }
+      public SubClass SubClassVal { get; set; }
+      public SubClass[] SubClassArr { get; set; }
+      public MyType() {
+         this.SubClassVal = new SubClass();
+      }
+   }
+
+   [TypeConverter(typeof(ExpandableObjectConverter))]
+   public class SubClass {
+      public int IntVal { get; set; }
+      public float FloatVal { get; set; }
+      public bool BoolVal { get; set; }
+      public string StringVal { get; set; }
+      public WeekDay WeekDay { get; set; }
+      public int[] IntArr { get; set; }
+      public string[] StringArr { get; set; }
+   }
+}
