afc6f37d17e4dedbae630d4331552d86462e83c3
sim511777 <sim51177@gmail.com>
2019-06-11 20:08:32 +0900

    Member Order

--- JsonTest/MyType.cs
+++ JsonTest/MyType.cs
@@ -6,39 +6,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Runtime.Serialization;
 
 namespace JsonTest {
-   public enum WeekDay {
-      Sunday, Monday, Tuesday, WednesDay, Thursday, Friday, Saturday,
-   }
+    public enum WeekDay {
+        Sunday, Monday, Tuesday, WednesDay, Thursday, Friday, Saturday,
+    }
 
-   public class MyType {
-      public int IntVal { get; set; }
-      public float FloatVal { get; set; }
-      public bool BoolVal { get; set; }
-      public string StringVal { get; set; }
-      public WeekDay WeekDay { get; set; }
-      public Point Point { get; set; }
-      public Color Color { get; set; }
-      public int[] IntArr { get; set; }
-      public string[] StringArr { get; set; }
-      public SubClass SubClassVal { get; set; }
-      public SubClass[] SubClassArr { get; set; }
-      public MyType() {
-         this.SubClassVal = new SubClass();
-      }
-   }
+    [DataContract]
+    public class MyType {
+        [DataMember(Order=1)] public int IntVal { get; set; }
+        [DataMember(Order=2)] public float FloatVal { get; set; }
+        [DataMember(Order=3)] public bool BoolVal { get; set; }
+        [DataMember(Order=4)] public string StringVal { get; set; }
+        [DataMember(Order=5)] public WeekDay WeekDay { get; set; }
+        [DataMember(Order=6)] public Point Point { get; set; }
+        [DataMember(Order=7)] public Color Color { get; set; }
+        [DataMember(Order=8)] public int[] IntArr { get; set; }
+        [DataMember(Order=9)] public string[] StringArr { get; set; }
+        [DataMember(Order=10)] public SubClass SubClassVal { get; set; }
+        [DataMember(Order=11)] public SubClass[] SubClassArr { get; set; }
+        public MyType() {
+            this.SubClassVal = new SubClass();
+        }
+    }
 
-   [TypeConverter(typeof(ExpandableObjectConverter))]
-   public class SubClass {
-      public int IntVal { get; set; }
-      public float FloatVal { get; set; }
-      public bool BoolVal { get; set; }
-      public string StringVal { get; set; }
-      public WeekDay WeekDay { get; set; }
-      public Point Point { get; set; }
-      public Color Color { get; set; }
-      public int[] IntArr { get; set; }
-      public string[] StringArr { get; set; }
-   }
+    [DataContract]
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    public class SubClass {
+        [DataMember(Order=1)] public int IntVal { get; set; }
+        [DataMember(Order=2)] public float FloatVal { get; set; }
+        [DataMember(Order=3)] public bool BoolVal { get; set; }
+        [DataMember(Order=4)] public string StringVal { get; set; }
+        [DataMember(Order=5)] public WeekDay WeekDay { get; set; }
+        [DataMember(Order=6)] public Point Point { get; set; }
+        [DataMember(Order=7)] public Color Color { get; set; }
+        [DataMember(Order=8)] public int[] IntArr { get; set; }
+        public string[] StringArr { get; set; }
+    }
 }
